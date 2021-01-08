0f429af115da6b69ab0bccfdbe0e7d4bce302793
Watt Wang <watt.wang@softtek.com>
2021-01-08 13:58:33 +0800

    Add project files.

---
+++ GitManager.CMD/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LibGit2Sharp;
+using LibGit2Sharp.Handlers;
+
+namespace GitManager.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入要查找的路径：");
+            var path = Console.ReadLine();
+
+            var repoPath = Repository.Discover(path);
+            var repo = new Repository(repoPath);
+            
+            
+            Console.WriteLine(repo.Info.Message);
+            Console.ReadKey();
+        }
+    }
+}
